d31a65b0ca1dfe98621d0c07948c95c2ed81640f
Rakesh29Ar <rakeshravi29296@gmail.com>
2020-05-02 23:42:24 +0530

    project

---
+++ Runner/Assets/Scripts/Ballimpulse.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballimpulse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float ballspeedz;
+
+    [SerializeField]
+    private Vector3 ballforce=new Vector3(0,3,3);
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ballspeedz = rb.velocity.z;
+    }
+
+    private void Update()
+    {
+        ballspeedz = rb.velocity.z;
+    }
+
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+            rb.Sleep();
+            ballforce = new Vector3(0,3,3);
+            rb.AddForce(ballforce, ForceMode.Impulse);
+        
+    }
+
+
+}
---
+++ Runner/Assets/Scripts/CameraCtrl_S.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl_S : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothspeed = 0.125f;
+    public Vector3 offset;
+
+    
+    
+
+    
+    private void FixedUpdate()
+    {
+        Vector3 desiredpos = target.position + offset;
+        Vector3 smoothedpos = Vector3.Lerp(transform.position, desiredpos, smoothspeed);
+        transform.position = smoothedpos;
+
+
+    }
+}
