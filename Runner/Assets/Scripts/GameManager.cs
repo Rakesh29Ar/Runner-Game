ac7a3c483c8a53298ba6f64a6e0f07bbe717e3f1
prem <prembobby12@gmail.com>
2020-05-06 22:50:23 +0530

    Modified by Prem

---
+++ Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + Time.deltaTime * speed);
+
+        if(transform.position.z > 477.0f)
+            transform.position = new Vector3(transform.position.x, transform.position.y, -7.0f);
+
+    }
+}
