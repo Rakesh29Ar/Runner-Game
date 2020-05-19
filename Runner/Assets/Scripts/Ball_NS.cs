e77b4a2d6d2e742e986220b8fcbd449e9524cabe
Rakesh29Ar <rakeshravi29296@gmail.com>
2020-05-19 15:02:30 +0530

    path follow

--- Runner/Assets/Scripts/Ball_NS.cs
+++ Runner/Assets/Scripts/Ball_NS.cs
@@ -8,11 +8,26 @@ public class Ball_NS : MonoBehaviour
     private Rigidbody BallRb;
 
     [SerializeField]
+    private float fallmultiplier;
+    [SerializeField]
+    private float lowjumpmultiplier;
+
+    [SerializeField]
     private float BallForceY;
     private void Start()
     {
         BallRb = GetComponent<Rigidbody>();
+        
     }
+
+    private void FixedUpdate()
+    {
+       /* if(BallRb.velocity.y<0)
+        {
+            BallRb.
+        }*/
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         BallRb.Sleep();
